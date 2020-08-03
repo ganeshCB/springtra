9eb4f598542af2a977384ed6517d7cc9452bb1c9
Lenovo <Lenovo@DESKTOP-4HQ1AI1>
2020-08-03 17:17:42 +0530

    Update

---
+++ rest-service/src/main/java/com/example/demo/RestServiceApplication.java
@@ -0,0 +1,38 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.example.demo.entity.CibilScore;
+import com.example.demo.repo.CibilScoreRepository;
+
+@SpringBootApplication
+public class RestServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestServiceApplication.class, args);
+		
+		
+	}
+
+	@Bean
+	public CommandLineRunner runner() {
+		
+		return new CommandLineRunner() {
+			
+			@Autowired
+			CibilScoreRepository repo;
+			@Override
+			public void run(String... args) throws Exception {
+				
+				repo.save(new CibilScore(101, "aa11", "Ramesh", 620));
+				
+				repo.save(new CibilScore(102, "bb22", "Suresh", 670));
+				
+			}
+		};
+}
+}
