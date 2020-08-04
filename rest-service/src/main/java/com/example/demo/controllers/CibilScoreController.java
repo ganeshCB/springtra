3c4c35387f043ea4fffae5456ff000be7be89819
Lenovo <Lenovo@DESKTOP-4HQ1AI1>
2020-08-04 12:32:01 +0530

    Hateos

--- rest-service/src/main/java/com/example/demo/controllers/CibilScoreController.java
+++ rest-service/src/main/java/com/example/demo/controllers/CibilScoreController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,7 +17,9 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.demo.entity.CibilScore;
 import com.example.demo.service.CibilScoreService;
 
+
 @RestController
+@CrossOrigin(origins = "*")
 public class CibilScoreController {
 
 	
